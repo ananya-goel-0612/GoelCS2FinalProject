51a06cd88174aa909cc821f363c2eb2e38fda7c0
ananya-goel-0612 <ananya.goel@menloschool.org>
2025-04-15 14:52:18 -0700

    added more methods to initialize the front end

--- src/KpopQuiz.java
+++ src/KpopQuiz.java
@@ -1,13 +1,62 @@
-public class KpopQuiz {
+import java.awt.event.*;
+
+public class KpopQuiz implements MouseListener, MouseMotionListener, ActionListener {
+
     private QuizManager quizManager;
     private KpopQuizView window;
 
+    public KpopQuiz() {
+        this.window = new KpopQuizView();
+
+        this.window.addMouseListener(this);
+        this.window.addMouseMotionListener(this);
+    }
 
     public void start() {
 
     }
 
     public static void main(String[] args) {
+        KpopQuiz quiz = new KpopQuiz();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        window.repaint();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
 
     }
 }
--- src/KpopQuizView.java
+++ src/KpopQuizView.java
@@ -1,8 +1,23 @@
-public class KpopQuizView {
+import javax.swing.*;
+import java.awt.*;
+
+public class KpopQuizView extends JFrame {
+    public static final int WINDOW_WIDTH = 700;
+    public static final int WINDOW_HEIGHT = 500;
+
+
     private QuizManager quizManager;
     //- questionLabel: JLabel
     //- buttons: List<JButton>
     //- scoreLabel: JLabel
+
+    public KpopQuizView() {
+        this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        this.setLocationRelativeTo(null);
+        this.setTitle("K-pop Quiz!");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
 
     public void displayQuestion(Question q) {
 
@@ -18,6 +33,11 @@ public class KpopQuizView {
 
     public void setupUI() {
 
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
     }
 
 }
